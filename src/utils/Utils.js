a133a34dfd012f6f2e8475446ba51bbbe923dce2
devLopesRenato <renatolopes37@gmail.com>
2024-02-17 00:59:45 -0300

    feat: Create new Utils

---
+++ src/utils/Utils.js
@@ -0,0 +1,49 @@
+const BadRequestError = require('../common/errors/types/BadRequestError')
+
+exports.isUUID = (str) => {
+    const uuidRegex = /^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$/i;
+    return uuidRegex.test(str);
+}
+
+exports.isNumber = (value) => {
+    if (!value) return false
+    if (String(value).replace(/[0-9.,]/g, '') === "") return true;
+
+    return false;
+}
+
+exports.paramsValidator = (paramTypes = [], params = {}) => {
+    for (const { name, type, rules } of paramTypes) {
+        const types = Array.isArray(type) ? type : [type];
+        if (!types.includes(typeof params[name])) {
+            throw new BadRequestError(`Invalid parameter ${name}. Expected: ${types.join(" or ")}, received: ${typeof params[name]}`);
+        }
+
+        if (rules && rules.length) {
+            for (const rule of rules) {
+                switch (rule) {
+                    case 'isNotEmpty':
+                        if (params[name] === "") {
+                            throw new BadRequestError(`The ${name} field cannot be empty`);
+                        }
+                        break;
+
+                    case 'isUUID':
+                        if (!exports.isUUID(params[name])) {
+                            throw new BadRequestError("Invalid uuid");
+                        }
+                        break;
+
+                    case 'isNumber':
+                        if (!exports.isNumber(params[name])) {
+                            throw new BadRequestError(`Invalid ${name}`);
+                        }
+                        break;
+
+                    default:
+                        break;
+                }
+            }
+        }
+    }
+}
