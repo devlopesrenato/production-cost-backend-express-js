9711c5951c7cc122b3535c7f59420e3220405a91
devLopesRenato <renatolopes37@gmail.com>
2024-02-18 02:14:47 -0300

    feat: AuthMiddleware check if the user is valid

--- src/AuthMiddleware/index.js
+++ src/AuthMiddleware/index.js
@@ -1,14 +1,16 @@
 const jwt = require('jsonwebtoken');
 const UnauthorizedError = require('../common/errors/types/UnauthorizedError');
 const InternalServerError = require('../common/errors/types/InternalServerError');
+const UsersRepository = require('../users/users.repository');
 require('dotenv').config()
 
 class AuthMiddleware {
     constructor() {
         this.secret = process.env.SECRET_KEY;
+        this.usersRepository = new UsersRepository();
     }
 
-    verifyJWT(req, next) {
+    async verifyJWT(req, next) {
         try {
             const token = req.headers.authorization;
             if (!token) {
@@ -20,11 +22,16 @@ class AuthMiddleware {
                     throw new UnauthorizedError("Invalid JWT token")
                 }
                 req.userId = decoded.id;
-                next()
             });
+            
+            const user = await this.usersRepository.getById(req.userId);
+            if (!user) {
+                throw new UnauthorizedError("Invalid JWT token")
+            }
         } catch (error) {
-            console.log(error)
-            throw new InternalServerError("Failed to verify token")
+            next(error)
+        } finally {
+            next()
         }
     }
 
