faf6b656e4a56c0a1e62d34eaff25f30dba2c1d3
devLopesRenato <renatolopes37@gmail.com>
2024-02-19 18:12:06 -0300

    fix: Remove unused files for reports

--- src/routes/reports.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const reports = require('../repositories/reports.repository');
-
-router.get('/costPerProduction', reports.getCostPerProduction);
-
-
-
-module.exports = router;
