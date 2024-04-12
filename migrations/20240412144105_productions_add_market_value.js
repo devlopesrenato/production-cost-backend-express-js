df33e04118b9d1ba950cd2aeaa08df691de35dcf
devLopesRenato <renatolopes37@gmail.com>
2024-04-12 11:55:24 -0300

    feat: create migration add field market price

---
+++ migrations/20240412144105_productions_add_market_value.js
@@ -0,0 +1,21 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+    return knex.schema
+        .alterTable('productions', function (table) {
+            table.float('marketPrice').defaultTo(1)
+        })
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {
+    return knex.schema
+        .alterTable('productions', function (table) {
+            table.dropColumn('marketPrice');
+        })
+};
